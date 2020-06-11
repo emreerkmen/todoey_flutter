620aac5ee7bead2b172730de5675d8caeace15ef
emreerkmen <emre.erkmen@hotmail.com>
2020-06-11 22:29:09 +0300

    this local state.

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,15 +1,33 @@
 import 'package:flutter/material.dart';
 
 class TaskTitle extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return ListTile(
       title: Text('This is a taskOne'),
-      trailing: Checkbox(
-        value: false,
-        onChanged: (bool) {},
-      ),
+      trailing: TaskCheckbox(),
+    );
+  }
+}
+
+class TaskCheckbox extends StatefulWidget {
+  @override
+  _TaskCheckboxState createState() => _TaskCheckboxState();
+}
+
+class _TaskCheckboxState extends State<TaskCheckbox> {
+  bool isChecked = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Colors.lightBlueAccent,
+      value: isChecked,
+      onChanged: (newValue) {
+        setState(() {
+          isChecked = newValue;
+        });
+      },
     );
   }
 }
